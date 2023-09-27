fd2bfbba0a3dada98c784ee577ca06f3f2af1517
Johnwav <johnwav@users.noreply.github.com>
2023-09-27 15:35:35 +0100

    Feat: Ui fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Box from "./components/Box/Box";
+import CustomQuestions from "./components/CustomQuestions/CustomQuestions";
 import PersonalInfo from "./components/PersonalInfo/PersonalInfo";
 import Profile from "./components/Profile/Profile";
 import UploadArea from "./components/UploadArea/UploadArea";
@@ -7,22 +8,15 @@ import UploadArea from "./components/UploadArea/UploadArea";
 function App() {
   return (
     <>
-      <Box title={"Upload cover image"}>
-        <div
-          style={{
-            paddingInline: "40px",
-            paddingTop: "60px",
-            paddingBottom: "56px",
-          }}
-        >
-          <UploadArea />
-        </div>
-      </Box>
+        <UploadArea />
       <Box title={"Personal Information"}>
         <PersonalInfo />
       </Box>
       <Box title={"Profile"}>
         <Profile />
+      </Box>
+      <Box title={"Addtional Questions"}>
+        <CustomQuestions />
       </Box>
     </>
   );
