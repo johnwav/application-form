a56cc3f1629bd27c91ca79f914047d4f0ae771b3
Johnwav <johnwav@users.noreply.github.com>
2023-09-27 17:08:56 +0100

    Feat: app style + put form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Box from "./components/Box/Box";
 import CustomQuestions from "./components/CustomQuestions/CustomQuestions";
 import PersonalInfo from "./components/PersonalInfo/PersonalInfo";
 import Profile from "./components/Profile/Profile";
 import UploadArea from "./components/UploadArea/UploadArea";
+import { updateData } from "./services/apiService";
+import { RootState } from "./store/store";
 
 function App() {
+  const Form = useSelector((state: RootState) => state.data);
+  const programId = "enterId"
+
+  const put = async () => {
+    const payload = Form; // Check the payload before sending
+    console.log("Payload:", payload);
+    if (Form.data.attributes.coverImage === "") {
+      alert("Please upload an image");
+      return;
+    }
+    await updateData(payload, programId);
+    alert('Form Created')
+  };
+
   return (
     <div className="app">
       <UploadArea />
@@ -18,6 +35,10 @@ function App() {
       <Box title={"Addtional Questions"}>
         <CustomQuestions />
       </Box>
+
+      <button className="create-form" onClick={put}>
+        <h3>Create Form</h3>
+      </button>
     </div>
   );
 }
