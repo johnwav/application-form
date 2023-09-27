e859a2d59d9493ea1227d1ef082592e3cbf7310e
Johnwav <johnwav@users.noreply.github.com>
2023-09-27 17:09:17 +0100

    Feat: api service file

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,31 +1,46 @@
-// src/apiService.ts
-import axios from 'axios';
+import axios from "axios";
+import { ApplicationFormState } from "../types/types";
 
-const API_BASE_URL = 'http://127.0.0.1:3100'; // Replace with your API URL
-
+// const API_BASE_URL = "http://127.0.0.1:4010"; // Replace with your API URL
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
-export const updateData = async (data: unknown) => {
+
+export const updateData = async (
+  data: ApplicationFormState,
+  programId: string
+): Promise<{
+  data: ApplicationFormState;
+}> => {
   // eslint-disable-next-line no-useless-catch
   try {
     // Replace '/api/update' with your actual API endpoint
-    const response = await api.put('/api/update', data);
+    const response = await api.put(
+      `/api/${import.meta.env.VITE_API_VERSION}/programs/${programId}/application-form`,
+      data
+    );
     return response.data;
   } catch (error) {
     throw error;
   }
 };
 
-export const fetchData = async () => {
+export const fetchData = async (
+  version: number,
+  programId: string
+): Promise<{
+  data: ApplicationFormState;
+}> => {
   // eslint-disable-next-line no-useless-catch
   try {
     // Replace '/api/data' with your actual API endpoint
-    const response = await api.get('/api/data');
+    const response = await api.get(
+      `/api/${version}/programs/${programId}/application-form`
+    );
     return response.data;
   } catch (error) {
     throw error;
